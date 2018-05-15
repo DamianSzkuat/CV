f1b048ba7d0ee3e48acfb7333685df86b5d48d43
DamianSzkuat <DamianSzkuat@hotmail.com>
2018-05-15 10:31:11 +0200

    restructure folders and start gui

---
+++ gui/mainwindow.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+
+class Ui_mainwindow:
+
+    def __init__(self, master):
+
+        # Build a frame and pack it to root/master
+        frame = tk.Frame(master)
+        frame.pack()
